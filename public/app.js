ecb9d4e0235e858ba6ddea6abb1e02d24cfd26a4
AxioDev <80776111+AxioDev@users.noreply.github.com>
2025-06-12 19:01:04 +0200

    Improve web voice relay

---
+++ public/app.js
@@ -0,0 +1,18 @@
+let ws;
+let recorder;
+
+async function start() {
+  ws = new WebSocket(`ws://${location.host}`);
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  recorder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
+  recorder.ondataavailable = e => { if (ws.readyState === 1) ws.send(e.data); };
+  recorder.start(250);
+}
+
+function stop() {
+  if (recorder) recorder.stop();
+  if (ws) ws.close();
+}
+
+document.getElementById('start').onclick = start;
+document.getElementById('stop').onclick = stop;
